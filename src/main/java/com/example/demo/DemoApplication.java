d34ad3847df4acd0090d1a6a4c8d57a252b49e9b
GeneJackett <genejackett@gmail.com>
2025-08-15 22:45:06 -0500

    Comment message

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+		var OrderService = context.getBean(OrderService.class);
+		OrderService.placeOrder();
 	}
 
 }
